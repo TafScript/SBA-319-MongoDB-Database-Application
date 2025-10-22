79129a8a93f535dee47355b06587596005fd699d
Tafari <Tafarigeorge5079@gmail.com>
2025-10-21 20:35:31 -0400

    Another Schema

--- models/Developer.js
+++ models/Developer.js
@@ -1,2 +1,14 @@
 import mongoose from 'mongoose';
 
+const DeveloperSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+})
+
+export default DeveloperSchema;
