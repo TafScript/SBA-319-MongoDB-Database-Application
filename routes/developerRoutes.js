43362888510f057d9f9071b5b213a4a54de64908
Tafari <Tafarigeorge5079@gmail.com>
2025-10-21 21:00:58 -0400

    Nth save

--- routes/developerRoutes.js
+++ routes/developerRoutes.js
@@ -1,3 +1,12 @@
 import express from 'express';
 const router = express.Router();
 import Developer from '../models/Developer.js'
+
+router.get('/', async (req, res) => {
+    try {
+        const developers = await Developer.find();
+        res.json(developers);
+    } catch (err) {
+        res.status(500).json({ error: "Failed to fetch developers" });
+    }
+})
